1e59fd857fe5ca551567b26fd6cb0f268c8bac24
Md Ariful Haque <arifulhaque02128@gmail.com>
2025-07-20 02:34:14 -0700

    updated booking routes

--- src/app/api/bookings/[id]/route.js
+++ src/app/api/bookings/[id]/route.js
@@ -17,11 +17,28 @@ export const PATCH = async (req, {params}) => {
     const {id} = await params;
 
     const payload = await req.json();
+    const session = await getServerSession(authOptions);
+
 
     const collection = await dbConnect("car-bookings");
-    const res = await collection.updateOne({_id : new ObjectId(id)}, {$set : payload});
+    const currentBooking = await collection.findOne({_id : new ObjectId(id)});
 
-    return  Response.json(res);
+    const targetEmail = currentBooking?.email;
+
+    const currentUserEmail = session?.user?.email;
+
+    if(targetEmail === currentUserEmail){
+        const res = await collection.updateOne({_id : new ObjectId(id)}, {$set : payload});
+        return Response.json(res);
+    }
+
+    const isAdmin = currentUserEmail === "ariiifhaque@gmail.com" || currentUserEmail === "jsmith@gmail.com";
+    if(isAdmin){
+        const res = await collection.updateOne({_id : new ObjectId(id)}, {$set : payload});
+        return Response.json(res);
+    }
+
+    return  Response.json({response : "Failed to Update"}, {status : 400});
 }
 
 export const DELETE = async (req, {params}) => {
@@ -32,11 +49,17 @@ export const DELETE = async (req, {params}) => {
     const collection = await dbConnect("car-bookings");
     const currentBooking = await collection.findOne({_id : new ObjectId(id)});
 
-    // console.log("CURRENT :::", currentBooking);
+    const targetEmail = currentBooking?.email;
 
-    // console.log("SESSION :::", session);
+    const currentUserEmail = session?.user?.email;
 
-    if(currentBooking?.email === session?.user?.email){
+    if(targetEmail === currentUserEmail){
+        const res = await collection.deleteOne({_id : new ObjectId(id)});
+        return Response.json(res);
+    }
+
+    const isAdmin = currentUserEmail === "ariiifhaque@gmail.com" || currentUserEmail === "jsmith@gmail.com";
+    if(isAdmin){
         const res = await collection.deleteOne({_id : new ObjectId(id)});
         return Response.json(res);
     }
